d8cf8f869e8d8341d1d7d9f791f65567f74303f6
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2020-04-10 16:58:27 -0500

    Deploying

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send("Deployed! try GET /filteredimage?image_url={{}}")
   } );
 
   // Start the Server
